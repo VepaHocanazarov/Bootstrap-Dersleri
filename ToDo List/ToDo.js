8c945d8630c4a53f785f31680bea4c5591fe7d81
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-03 14:09:51 +0300

    Bootsrap Dersleri

---
+++ ToDo List/ToDo.js
@@ -0,0 +1,95 @@
+
+var items = ["item 1", "item 2", "item 3", "item 4"];
+
+
+
+var list = document.querySelector("#myList");
+
+items.forEach(function (item) {
+
+CreateItem(item);
+
+})
+
+
+list.addEventListener("click", function (item) {
+
+    if (item.target.tagName = "LI") {
+        item.target.classList.toggle("checked");
+
+        ToggleDeleteButton();
+        
+    }
+});
+
+document.querySelector("#deleteAll").onclick=function(){
+
+    var elements = document.querySelectorAll(".checked");
+
+    elements.forEach(function(item){
+
+        item.style.display = "none";
+
+    });
+
+
+   
+
+}
+
+function ToggleDeleteButton(){
+    var checkList = document.querySelectorAll(".list-group-item.checked");
+
+    if(checkList.length>0){
+        document.querySelector("#deleteAll").classList.remove("d-none");
+    }
+    else{
+        document.querySelector("#deleteAll").classList.add("d-none");
+    }
+
+    //console.log(checkList.length);
+
+
+}
+
+
+
+
+document.querySelector("#btnCreate").onclick = function () {
+
+    var item = document.querySelector("#txtItem").value;
+
+    if (item === "") {
+        alert("Lütfen Alan Doldurunuz...");
+        return;
+
+    }
+
+    else {
+        
+        CreateItem(item);
+
+    }
+}
+
+function CreateItem(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+
+    li.className = "list-group-item";
+    li.appendChild(text);
+    list.appendChild(li);
+
+    var span = document.createElement("span");
+    var text = document.createTextNode("X");
+    span.className = "close"
+    span.appendChild(text);
+    li.appendChild(span);
+
+    span.onclick = function () {
+        var li = this.parentElement;
+        li.style.display = "none";
+        li.classList.remove("checked");
+    }
+
+}
